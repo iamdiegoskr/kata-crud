eb6c47ff162a38d43106c0828ff5dd8406c2e993
Diego Martinez <juandiego337@hotmail.com>
2021-09-12 14:08:36 -0500

    Initilize the project kata-crud

---
+++ src/test/java/com/sofkau/katacrud/KataCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkau.katacrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
