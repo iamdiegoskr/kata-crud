964761f9077264f6c1a1f4ed0da4125375e901c5
Diego Martinez <juandiego337@hotmail.com>
2021-09-12 14:16:46 -0500

    Entity Todo has been created

---
+++ src/main/java/com/sofkau/katacrud/entities/Todo.java
@@ -0,0 +1,42 @@
+package com.sofkau.katacrud.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Todo")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column()
+    private String name;
+
+    @Column()
+    private boolean isCompleted;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+}
